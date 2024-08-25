2c8d1ccf7c3396247f5b4e1f229d296cacca660f
sanin <saninthottungalhere@gmail.com>
2024-08-25 16:09:04 +0530

    losing state

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class AboutPage extends StatelessWidget {
+class AboutPage extends HookWidget {
   const AboutPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("About Page"),
+    final textState = useState("Loading");
+    useEffect(
+      () {
+        Future.delayed(
+          const Duration(seconds: 2),
+          () {
+            textState.value = 'About page';
+          },
+        );
+        return;
+      },
+    );
+    return Center(
+      child: Text(textState.value),
     );
   }
 }
